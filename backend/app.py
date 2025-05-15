9c382d64718853051567bee02c75176091da610f
Kartik Dixit <kartikdixit2468@gmail.com>
2025-05-16 04:58:32 +0530

    Added alternative routes for both of the models

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from flask import Flask
+from flask_cors import CORS
 import pandas as pd
 from joblib import load
 from utils.preprocess import preprocess_data
@@ -7,12 +9,8 @@ import time
 import json
 
 app = Flask(__name__)
-model = load('model/model.pkl')
+CORS(app)  # This enables CORS for all routes and origins
 
-# Load accuracy (R² score) from JSON
-with open('model/metrics.json', 'r') as f:
-    metrics = json.load(f)
-    model_r2 = round(metrics.get('r2', 0), 4)  # Default to 0 if not found
 
 @app.route('/api/', methods=['GET'])
 def api():
@@ -23,9 +21,15 @@ def api():
 @app.route('/api/model/xgboost/predict/', methods=['POST'])
 def predict_xgboost():
     print("Catched one request.")
-    data = request.get_json()
-    df = pd.DataFrame([data])
-    df = preprocess_data(df)
+
+    model = load('model/model_xgboost.pkl')
+    # Load accuracy (R² score) from JSON
+    with open('model/metrics_xgboost.json', 'r') as f:
+        metrics = json.load(f)
+        model_r2 = round(metrics.get('r2', 0), 4)  # Default to 0 if not found
+        data = request.get_json()
+        df = pd.DataFrame([data])
+        df = preprocess_data(df)
 
     # ✅ Fix: Add weight column with default value
     df['weight'] = 1
@@ -45,6 +49,16 @@ def predict_xgboost():
 @app.route('/api/model/randomforest/predict/', methods=['POST'])
 def predict_randomforest():
     print("Catched one request.")
+
+    model = load('model/model_randomforest.pkl')
+    # Load accuracy (R² score) from JSON
+    with open('model/metrics_randomforest.json', 'r') as f:
+        metrics = json.load(f)
+        model_r2 = round(metrics.get('r2', 0), 4)  # Default to 0 if not found
+        data = request.get_json()
+        df = pd.DataFrame([data])
+        df = preprocess_data(df)
+
     data = request.get_json()
     df = pd.DataFrame([data])
     df = preprocess_data(df)
